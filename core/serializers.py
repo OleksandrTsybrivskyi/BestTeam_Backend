28136d8031592b380cda04d2a197f8d9dbfbccf7
Oleksandr Tsybrivskyi <tsybrivsky@gmail.com>
2025-04-10 22:54:06 +0300

    feat(core/serializers.py): add serializers for models
    This update adds serializers for User, Location, Review
    models

---
+++ core/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import User, Location, Review
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'is_accessibility_user']
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = ['id', 'location', 'user', 'rating', 'comment', 'created_at']
