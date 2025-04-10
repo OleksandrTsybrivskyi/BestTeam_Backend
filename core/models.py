757f9ea27c9fa777b8d1f72dd6810be54c82a7c5
Oleksandr Tsybrivskyi <tsybrivsky@gmail.com>
2025-04-10 17:55:38 +0300

    feat(core/models.py, core/migrations): create models for database and make migrations
    This update adds User, Location, Review models to the database
    and makes migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_accessibility_user = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    address = models.CharField(max_length=255)
+    
+    has_ramp = models.BooleanField(default=False)
+    has_tactile_elements = models.BooleanField(default=False)
+    has_adapted_toilet = models.BooleanField(default=False)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    
+    rating = models.IntegerField(default=0)
+    comment = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Відгук {self.user.username} для {self.location.name}"
