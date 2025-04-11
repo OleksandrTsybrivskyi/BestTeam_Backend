324208dfeef1c7a3c70e219d453d8d67065eae23
Oleksandr Tsybrivskyi <tsybrivsky@gmail.com>
2025-04-11 18:03:49 +0300

    feat(core/views.py): merge LocationPostView and LocationGetView into class LocationView

--- core/views.py
+++ core/views.py
@@ -5,19 +5,15 @@ from .models import Location
 from .serializers import LocationSerializer
 
 
-class LocationGetView(APIView):
+class LocationView(APIView):
     def get(self, request):
         locations = Location.objects.filter(has_ramp=True)
-        print(locations)
         serialized = LocationSerializer(locations, many=True)
         return Response(serialized.data)
 
-class LocationPostView(APIView):
     def post(self, request):
-        print(request.data)
         serializer = LocationSerializer(data=request.data)
         if serializer.is_valid():
             location = serializer.save()
-            print(location.name)
             return Response(serializer.data)
         return Response(serializer.errors, status=400)
