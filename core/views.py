ca8416ed2ecd5c2c485adc9c1ebe2a9748d95c70
Oleksandr Tsybrivskyi <tsybrivsky@gmail.com>
2025-04-11 17:45:18 +0300

    feat(core/views.py): add Location GET and POST views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Location
+from .serializers import LocationSerializer
 
-# Create your views here.
+
+class LocationGetView(APIView):
+    def get(self, request):
+        locations = Location.objects.filter(has_ramp=True)
+        print(locations)
+        serialized = LocationSerializer(locations, many=True)
+        return Response(serialized.data)
+
+class LocationPostView(APIView):
+    def post(self, request):
+        print(request.data)
+        serializer = LocationSerializer(data=request.data)
+        if serializer.is_valid():
+            location = serializer.save()
+            print(location.name)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
