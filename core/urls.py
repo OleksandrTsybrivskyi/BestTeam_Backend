aae836d8bdd9758688cbb0e2059eee92a49013be
Oleksandr Tsybrivskyi <tsybrivsky@gmail.com>
2025-04-12 19:21:36 +0300

    feat(core/models.py, core/urls.py, core/views.py, mysite/settings.py): add authentication mechanism

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import LocationView, ReviewView
+from .views import LocationView, ReviewView, RegisterView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('locations/', LocationView.as_view(), name='locations-list'),
     path('reviews/', ReviewView.as_view(), name='reviews-list'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', obtain_auth_token, name='login')
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Location
+from .models import Location, Review, User
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+
 from .serializers import LocationSerializer
 from .lib import location_process_post, location_process_get, review_process_get, review_process_post
 
 
 class LocationView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         response = location_process_get(request)
         return Response(response)
@@ -17,6 +22,8 @@ class LocationView(APIView):
 
 
 class ReviewView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         response = review_process_get(request)
         return Response(response)
@@ -24,3 +31,20 @@ class ReviewView(APIView):
     def post(self, request):
         response = review_process_post(request)
         return Response(response)
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        data = request.data
+        username = data['username']
+        email = data['email']
+        password = data['password']
+        if not username or not email or not password:
+            return Response({'error': 'Missing fields'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'User with such username exists'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return Response({'error': 'User with such email exists'}, status=400)
+        user = User.objects.create(username=username, email=email, password=password)
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key}, status=201)
