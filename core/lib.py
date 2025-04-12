b70b2c4b25237da7ed0b9dd24ae8e834e1b562f6
mrboombastik_31 <timchik.oleksandr@gmail.com>
2025-04-12 21:03:31 +0300

    addded logic for reviews GET and POST

--- core/lib.py
+++ core/lib.py
@@ -1,13 +1,16 @@
 from .models import Location
-from .serializers import LocationSerializer
+from .serializers import LocationSerializer, ReviewSerializer
+from .models import User, Review
 
 
 def location_process_post(request):
     parameters = request.GET.dict()
 
 
+
 def location_process_get(request):
     data = request.data
+
 
 
 def review_process_get(request):
@@ -19,7 +22,24 @@ def review_process_get(request):
     У вигляді словників (типу словник в такому форматі, як ти його витягуєш з бази даних, тобто дата надсилання локація юзер і т д)
     Якщо ключа location_name немає в словнику, то поверни пустий список
     '''
+
+
     parameters = request.GET.dict()
+
+    location_name = parameters.get('location_name')
+
+    if not location_name:
+        return []
+
+    try:
+        location = Location.objects.get(name=location_name)
+    except Location.DoesNotExist:
+        return []
+
+    reviews = Review.objects.filter(location=location)
+    serialized_reviews = ReviewSerializer(reviews, many=True)
+
+    return serialized_reviews.data
 
 
 def review_process_post(request):
@@ -28,4 +48,28 @@ def review_process_post(request):
     потрібно дістати з нього всі потрібні дані щоб створити
     новий відгук, на основі того, що є в базі даних
     '''
+
     data = request.data
+
+    location_id = data.get('location')
+    user_id = data.get('user')
+    rating = data.get('rating')
+    comment = data.get('comment', '')
+
+    if not all([location_id, user_id, rating]):
+        return {"Помилка": "Нема даних полів."}
+
+    try:
+        location = Location.objects.get(id=location_id)
+        user = User.objects.get(id=user_id)
+    except (Location.DoesNotExist, User.DoesNotExist):
+        return {"Помилка": "Нечітка локація юзера або ID."}
+
+    review = Review.objects.create(
+        location=location,
+        user=user,
+        rating=rating,
+        comment=comment
+    )
+
+    return ReviewSerializer(review).data
