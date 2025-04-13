ffac837ba4595e6a964fa1cc790e9b880d97b051
Oleksandr Tsybrivskyi <tsybrivsky@gmail.com>
2025-04-13 11:45:45 +0300

    fix(core/serializers.py): fix UserSerializer password leakage and add all the necessary fields

--- core/serializers.py
+++ core/serializers.py
@@ -2,9 +2,10 @@ from rest_framework import serializers
 from .models import User, Location, Review
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['id', 'username', 'is_accessibility_user']
+        fields = ['id', 'username', 'email', 'password', 'join_date', 'is_accessibility_user']
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
