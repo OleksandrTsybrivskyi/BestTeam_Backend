7b1abdc920a7313256971af931e5a0632fbc15d6
bropy <bohdandesna22@gmail.com>
2025-04-13 23:53:39 +0300

    fix(settings.py): allow all hosts in development environment

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yamct8y)1$5bc@boyhxh+3nw6sknwsa^kbppb9d6@pgr=xl%zc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
