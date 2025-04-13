2b0ca039922da6c15f60ecc3298e476513c99042
Oleksandr Tsybrivskyi <tsybrivsky@gmail.com>
2025-04-13 10:44:27 +0300

    docs(core/lib.py): add docstrings for location_process_get() and location_process_post() functions

--- core/lib.py
+++ core/lib.py
@@ -2,12 +2,68 @@ from .models import Location
 from .serializers import LocationSerializer
 
 
-def location_process_post(request):
+def location_process_get(request):
+    '''
+    Видати список локацій з бази даних, що відповідають
+    заданим фільтрам
+
+    :param parameters: словник, що містить критерії фільтрації,
+    а саме:
+    ramps
+    tactile_elements
+    adapted_toilets
+    wide_entrance
+    visual_impairment_friendly
+    wheelchair_accessible
+    якщо якогось значення немає в словнику, вважати, що воно
+    дорівнює False
+
+    :return: список локацій у вигляді словників.
+    формат словника є таким:
+    {
+        id: number;
+        name: string;
+        position: [number, number];
+        accessibility: {
+            ramps: boolean;
+            tactileElements: boolean;
+            adaptedToilets: boolean;
+            wideEntrance: boolean;
+            visualImpairmentFriendly: boolean;
+            wheelchairAccessible: boolean;
+        };
+    };
+    '''
     parameters = request.GET.dict()
 
 
-def location_process_get(request):
+def location_process_post(request):
+    '''
+    Змінити параметри доступності певної локації
+
+    :param data: словник з даними про локацію
+    Формат словника
+    {
+        id: number;
+        name: string;
+        position: [number, number];
+        accessibility: {
+            ramps: boolean;
+            tactileElements: boolean;
+            adaptedToilets: boolean;
+            wideEntrance: boolean;
+            visualImpairmentFriendly: boolean;
+            wheelchairAccessible: boolean;
+        };
+    };
+    :param user: обєкт класу User, якщо is_accessibility_user == False
+    він не може змінити параметри доступності локації
+
+    :return: словник з локацією
+    якщо виникла помилка, повернути повідомлення про помилку
+    '''
     data = request.data
+    user = request.user
 
 
 def review_process_get(request):
